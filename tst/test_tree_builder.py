3668cae88745e53a5d221c4f6bf22e0f99905ac5
Fynn Freyer <fynn.freyer@protonmail.com>
2022-11-26 22:30:06 +0100

    extract the FileTreeBuilder test to a different file and amend the expected number of dirs to the right amount

---
+++ tst/test_tree_builder.py
@@ -0,0 +1,9 @@
+from subprocess import run
+
+
+def test_tree_builder(tree_builder):
+    root = tree_builder.from_yaml('assets/test_traversal.yaml')
+    proc = run(['tree', '-a', str(root)], capture_output=True, check=True)
+    out = proc.stdout.decode()
+    assert '13 directories, 12 files' in out.splitlines()[-1]
+

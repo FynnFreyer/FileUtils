8b48ab88fe1f3857b97753261cbf9b96fadbae08
Fynn Freyer <fynn.freyer@protonmail.com>
2022-11-27 17:17:59 +0100

    more tests for file traversal

--- tst/test_traversal.py
+++ tst/test_traversal.py
@@ -2,55 +2,70 @@ from util.traversal import traverse_file_tree, translate_glob_patterns, compile_
 
 
 class TestFileTreeTraversal:
-    """
-    # file and dir here and in sub dirs
-    anywhere_ignored_file
-    anywhere_ignored_dir/
-    """
-
     def test_gitignore_excludes_basic_pattern_in_topdir(self, file_tree_with_gitignore):
         results = traverse_file_tree(file_tree_with_gitignore, include_gitignore=True)
-
         assert file_tree_with_gitignore / 'anywhere_ignored_file' not in results
         assert file_tree_with_gitignore / 'anywhere_ignored_dir' not in results
 
     def test_gitignore_excludes_basic_pattern_in_subdir(self, file_tree_with_gitignore):
-        """
-        # file and dir here and in sub dirs
-        anywhere_ignored_file
-        anywhere_ignored_dir/
-        """
         results = traverse_file_tree(file_tree_with_gitignore, include_gitignore=True)
-
         assert file_tree_with_gitignore / 'not_ignored_dir' / 'anywhere_ignored_file' not in results
         assert file_tree_with_gitignore / 'not_ignored_dir' / 'anywhere_ignored_dir' not in results
 
     def test_gitignore_excludes_top_level_pattern_in_topdir(self, file_tree_with_gitignore):
         results = traverse_file_tree(file_tree_with_gitignore, include_gitignore=True)
-
         assert file_tree_with_gitignore / 'toplevel_ignored_file' not in results
         assert file_tree_with_gitignore / 'toplevel_ignored_dir' not in results
 
     def test_gitignore_keeps_top_level_pattern_in_subdir(self, file_tree_with_gitignore):
         results = traverse_file_tree(file_tree_with_gitignore, include_gitignore=True)
-
-        assert file_tree_with_gitignore / 'toplevel_ignored_file' not in results
-        assert file_tree_with_gitignore / 'toplevel_ignored_dir' not in results
-
         assert file_tree_with_gitignore / 'not_ignored_dir' / 'toplevel_ignored_file' in results
         assert file_tree_with_gitignore / 'not_ignored_dir' / 'toplevel_ignored_dir' in results
 
-    def test_regard_pattern_excludes_unregarded(self, file_tree):
-        assert False
+    def test_gitignore_middle_slash_causes_top_level_pattern(self, file_tree_with_gitignore):
+        results = traverse_file_tree(file_tree_with_gitignore, include_gitignore=True)
+        assert file_tree_with_gitignore / 'another' / 'toplevel_ignored_file' not in results
+        assert file_tree_with_gitignore / 'another' / 'toplevel_ignored_dir' not in results
+        # should only exclude the pattern at top level
+        assert file_tree_with_gitignore / 'another' / 'another' / 'toplevel_ignored_file' in results
+        assert file_tree_with_gitignore / 'another' / 'another' / 'toplevel_ignored_dir' in results
 
-    def test_regard_pattern_includes_regarded(self, file_tree):
-        assert False
+    def test_basic_regard_pattern_includes_regarded_files(self, file_tree):
+        results = traverse_file_tree(file_tree, regard_patterns=['not_ignored_file'])
+        assert file_tree / 'not_ignored_file' in results
 
-    def test_ignore_pattern_excludes_ignored(self, file_tree):
-        assert False
+    def test_basic_regard_pattern_excludes_unregarded_files(self, file_tree):
+        results = traverse_file_tree(file_tree, regard_patterns=['not_ignored_file'])
+        assert file_tree / 'anywhere_ignored_file' not in results
 
-    def test_ignore_pattern_includes_unignored(self, file_tree):
-        assert False
+    def test_basic_regard_pattern_includes_unregarded_dirs(self, file_tree):
+        results = traverse_file_tree(file_tree, regard_patterns=['not_ignored_file'])
+        assert file_tree / 'not_ignored_dir' in results
+
+    def test_basic_regard_pattern_excludes_unregarded_dirs_with_concerns_dirs_option(self, file_tree):
+        results = traverse_file_tree(file_tree, regard_patterns=['not_ignored_file'], regard_patterns_concern_dirs=True)
+        assert file_tree / 'not_ignored_dir' not in results
+
+    def test_regard_pattern_includes_files_with_glob(self, file_tree):
+        results = traverse_file_tree(file_tree, regard_patterns=['not_ignored_*'])
+        assert file_tree / 'not_ignored_file' in results
+
+    def test_regard_pattern_includes_dirs_with_glob_and_concerns_dirs(self, file_tree):
+        results = traverse_file_tree(file_tree, regard_patterns=['not_ignored_*'], regard_patterns_concern_dirs=True)
+        assert file_tree / 'not_ignored_dir' in results
+
+    def test_ignore_pattern_excludes_ignored_in_toplevel(self, file_tree):
+        results = traverse_file_tree(file_tree, ignore_patterns=['anywhere_ignored_file'])
+        assert file_tree / 'anywhere_ignored_file' not in results
+
+    def test_ignore_pattern_excludes_ignored_in_subdir(self, file_tree):
+        results = traverse_file_tree(file_tree, ignore_patterns=['anywhere_ignored_file'])
+        assert file_tree / 'not_ignored_dir' / 'anywhere_ignored_file' not in results
+
+    def test_ignore_pattern_includes_unignored_files_and_dirs(self, file_tree):
+        results = traverse_file_tree(file_tree, ignore_patterns=['anywhere_ignored_file'])
+        assert file_tree / 'not_ignored_file' in results
+        assert file_tree / 'not_ignored_dir' in results
 
 
 class TestGlobPatterns:
@@ -61,12 +76,14 @@ class TestGlobPatterns:
 
     def test_double_asterisk_matches_sub_dir(self):
         pattern = compile_glob_patterns(['/path/to/**/foo'])[0]
-        # not just '/path/to//foo'
+        assert pattern.match('/path/to/another/foo')
+
+    def test_single_asterisk_matches_subdir(self):
+        pattern = compile_glob_patterns(['/path/to/*/foo'])[0]
         assert pattern.match('/path/to/another/foo')
 
     def test_single_asterisk_respects_separators(self):
         pattern = compile_glob_patterns(['/path/to/*/foo'])[0]
-        assert pattern.match('/path/to/another/foo')
         assert not pattern.match('/path/to/yet/another/foo')
 
     def test_patterns_dont_extend_past_definition(self):
